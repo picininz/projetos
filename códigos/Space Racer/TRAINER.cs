3d7b37a90b4516e8d53ac9da1adc1bd368575d70
felipe picinin <138057342+picininz@users.noreply.github.com>
2023-08-22 18:10:34 -0300

    Update TRAINER.cs

--- códigos/Space Racer/TRAINER.cs
+++ códigos/Space Racer/TRAINER.cs
@@ -1,54 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-public class NewCar: MonoBehaviour
+
+
+public class TRAINER : MonoBehaviour
 {
-    private float moveInput, turnInput;
-    public float fwdSpeed, revSpeed, turnSpeed;
-    public Rigidbody sphereRB;
-    float time;
+
+    public int score;
+    public Text txtScore;
+    private float timeLevel;
+    public Text timeLevel_txt;
     
-
-    float[] speeds = { 1.0f, 1.3f, 1.5f, 1.6f };
-    int testMarcha = 0;
-  
-
+    
 
     void Start()
     {
-        sphereRB.transform.parent = null;
+
     }
+
     void Update()
     {
-        moveInput = Input.GetAxisRaw("Vertical");
+        timeLevel = timeLevel + Time.deltaTime;
+        timeLevel_txt.text = timeLevel.ToString("TEMPO: 0.00");
 
-        turnInput = Input.GetAxisRaw("Horizontal");
-
-        moveInput *= moveInput > 0 ? fwdSpeed : revSpeed;
-
-        transform.position = sphereRB.transform.position;
-
-        float newRotation = turnInput * turnSpeed * Time.deltaTime * Input.GetAxisRaw("Vertical");
-        
-        transform.Rotate(0, newRotation, 0, Space.World);
-     
     }
-    private void FixedUpdate()
+
+    
+    void OnTriggerEnter(Collider other)
     {
-       if (moveInput > 0.1f)
-       {
-            time += Time.fixedDeltaTime;
-            
-       }
-       else
-       {
-            time -= Time.fixedDeltaTime;
-       }
-        if (time > 3) time = 3;
-        if (time < 0) time = 0;
-        testMarcha = (int)time;
-     
-        sphereRB.AddForce(transform.forward * speeds[testMarcha] * moveInput, ForceMode.Acceleration);
-    }
+        if (other.tag == "Player")
+        {
+            score = score + 1;
+            txtScore.text = "VOLTAS: " + score;
+           
 
-   
+        }
+    }
 }
